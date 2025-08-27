00f2153198573375d749ee39495ba38b9c11ac6f
MUKESHKUMAR M <mukeshkumar.cse24@gmail.com>
2025-08-27 23:39:24 +0530

    Configure for GitHub Pages hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://your-github-username.github.io', // <-- Replace with your username
+  base: '/powerenv-technologies', // <-- This is your repository name
+});
