30e2f8366cada93ae1b3a33db30a98b15e3bf0bf
MUKESHKUMAR M <mukeshkumar.cse24@gmail.com>
2025-08-28 00:13:22 +0530

    config changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://your-github-username.github.io', // <-- Replace with your username
+  site: 'https://mukesh-dev-git.github.io', // <-- Replace with your username
   base: '/powerenv-technologies', // <-- This is your repository name
 });
